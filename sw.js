5b45329a51b3dfee24c26bf04dc7c3e8c3a06130
mm151 <mm151@og4-pool-23.ub.privat>
2016-12-27 08:55:40 +0100

    +ServiceWorker

---
+++ sw.js
@@ -0,0 +1,41 @@
+/*global caches, fetch, Promise */
+(function (worker) {
+"use strict";
+
+var VERSION = 'v2.1',
+	FILES = [
+		'app.js',
+		'index.html',
+		'scream.ogg',
+		'xkcd_phone.png'
+	];
+
+worker.addEventListener('install', function (e) {
+	e.waitUntil(
+		caches.open(VERSION).then(function (cache) {
+			return cache.addAll(FILES);
+		})
+	);
+});
+
+worker.addEventListener('activate', function (e) {
+	e.waitUntil(
+		caches.keys().then(function (keys) {
+			return Promise.all(keys.map(function (key) {
+				if (key !== VERSION) {
+					return caches.delete(key);
+				}
+			}));
+		})
+	);
+});
+
+worker.addEventListener('fetch', function (e) {
+	e.respondWith(caches.match(e.request, {ignoreSearch: true})
+		.then(function (response) {
+			return response || fetch(e.request);
+		})
+	);
+});
+
+})(this);
